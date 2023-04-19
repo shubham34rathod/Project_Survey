7419885db3ea167e070fd6bab93634a08c053042
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-04-19 22:09:16 +0530

    Created preview page component

---
+++ FrontEnd/src/Pages/PreviewSurvey.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import '../styles/preview-survey.css'
+
+
+export default function PreviewSurvey(){
+    return <>
+    <div className='container'>
+        <Sidebar />
+        <div className='list-container'>
+            <header className='list-header '>
+                <div id='searchform'>
+                    <button id='arrow'> &larr;</button>
+                        <h5>Preview</h5>
+                </div>
+                <div className='util'>
+                <div id="close-prev-btn">
+                            <button>Close Preview</button>
+                        </div>
+                        <div id="save-btn">
+                            <button>Save</button>
+                        </div>
+                </div>
+            </header>
+            <div className='survey-list'>
+               {/* <div className='question'>
+                    <h4>Question 1</h4>
+                    <div className='prev-form-container'>
+                        <form>
+                            <label>Question i'll be here</label>
+                            <div className='radio-container'>
+                                <input type="radio" />
+                                <input type="radio" />
+                                <input type="radio" />
+                            </div>
+                        </form>
+                    </div>
+                    
+               </div> */}
+               <div className='question'>
+                    <h4>Question 1</h4>
+                    <div className='prev-form-container'>
+                        <form>
+                            <label htmlFor='question'>Question i'll be here</label>
+                            <div id='question' className='radio-container'>
+                                <input id='opt-1' type="radio" value={1} name='q'/>
+                                <label htmlFor='op1-1'>Option 1</label>
+                                <input id='opt-2' type="radio" value={2} name='q' />
+                                <label htmlFor='op1-2'>Option 2</label>
+                                <input id='opt-3' type="radio" value={3} name='q' />
+                                <label htmlFor='op1-3'>Option 3</label>
+                            </div>
+                        </form>
+                    </div>
+                    
+               </div>
+            </div>
+            
+        </div>
+    </div>
+</>
+}
