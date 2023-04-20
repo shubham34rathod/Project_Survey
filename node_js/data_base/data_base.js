dbd32ddfe442c1bbbd217652b2c40e5116cb3ad9
shubham34rathod <shubham34rathod@gmail.com>
2023-04-20 11:25:54 +0530

    cookie added in nodejs

--- node_js/data_base/data_base.js
+++ node_js/data_base/data_base.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.Db_URL + process.env.Data_Base)
 .then(()=>console.log("connected to database"))
 .catch(()=>console.log("connection error"))
 
+//registration schema...........
+
 let Schema1=new mongoose.Schema({
     name:{type:String},
     email:{type:String},
@@ -15,4 +17,19 @@ let Schema1=new mongoose.Schema({
 
 let Model1=mongoose.model("storage",Schema1);
 
-module.exports={Model1}
+//Survey data schema............
+
+let Schema2=new mongoose.Schema({
+    name:{type:String},
+    description:{type:String},
+    typeOfSurvey:{type:String},
+    startDate:{type:String},
+    endDate:{type:String},
+    otherCriteria:{type:String},
+    imageName:{type:String}
+})
+
+let Model2=mongoose.model("survey_data",Schema2)
+
+
+module.exports={Model1,Model2}
--- node_js/router/user.js
+++ node_js/router/user.js
@@ -1,12 +1,58 @@
 const express=require('express');
+const path=require("path");
+const cors=require('cors')
+let jwt=require('jsonwebtoken');
 const bcrypt=require("bcrypt");
 require('dotenv').config()
-const {Model1}=require("../data_base/data_base.js")
+const multer=require("multer")
+const cookieParser = require('cookie-parser');
+const {Model1, Model2}=require("../data_base/data_base.js");
+const MulterGridfsStorage = require('multer-gridfs-storage');
 
 const router=express.Router();
 
+router.use(cors())
+
+router.use(cookieParser());
+
 router.use(express.json());
 router.use(express.urlencoded({extended:true}))
+
+//multer storage
+let storage=new MulterGridfsStorage.GridFsStorage({
+    url: process.env.Db_URL + process.env.Data_Base,
+    file: function(req,file){
+          return{
+            bucketName:"survey_img",
+            fileName:`${Date.now()}_${file.originalname}`
+          }
+    }
+})
+
+let upload=multer({storage:storage})
+
+//checking token 
+
+async function verifyToken(req,res,next){
+    let get_token=await req.cookies?.uid;
+
+    if(!get_token)
+    {
+        res.redirect("/login")
+    }
+    else
+    {
+        await jwt.verify(get_token, process.env.secret_key, (error,valid)=>{
+            if(error){
+                res.redirect("/login")
+            }
+            else{
+                next();
+            }
+        })
+    }
+    
+}
 
 router.get("/",(req,res)=>{
     res.send("Hello World")
@@ -31,6 +77,7 @@ router.post("/register",async (req,res)=>{
             })
             await doc1.save();
             res.send("data received")
+            
         }
         else
         {
@@ -53,6 +100,8 @@ router.post("/login",async (req,res)=>{
             let match=await bcrypt.compare(password,data.password)
             if(match)
             {
+                let token=await jwt.sign({use:email},process.env.secret_key)
+                await res.cookie("uid",token)
                 res.send("login successfully")
             }
             else
@@ -65,6 +114,28 @@ router.post("/login",async (req,res)=>{
             res.send("not registered")
         }
       
+    } 
+    catch (error) 
+    {
+        res.send(error)
+    }
+})
+
+router.post("/survey_data",verifyToken,upload.single("image"),(req,res)=>{
+    try 
+    {
+        console.log(req.body);
+        let {name,description,typeOfSurvey,startDate,endDate,otherCriteria,imageName}=req.body;
+        let doc2=new Model2({
+            name:name,
+            description:description,
+            typeOfSurvey:typeOfSurvey,
+            startDate:startDate,
+            endDate:endDate,
+            otherCriteria:otherCriteria,
+            imageName:req.file.filename
+        })
+        doc2.save();
     } 
     catch (error) 
     {
