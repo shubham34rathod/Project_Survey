701118e2ee317e589993b51f17aae652fb687325
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-04-21 13:48:13 +0530

    Image upload with preview

--- FrontEnd/src/Pages/CreateSurvey.js
+++ FrontEnd/src/Pages/CreateSurvey.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import '../styles/surveylist.css'
 import '../styles/create-survey.css'
 import '../styles/header.css'
 import { useNavigate } from "react-router-dom";
 
+
+
 export default function CreateSurvey() {
-const navigate = useNavigate()
+    const navigate = useNavigate()
+    const [img, setImg] = useState({
+        img: "",
+        name:""
+    });
+    function handleChange(e) {
+        setImg({
+            img: URL.createObjectURL(e.target.files[0]),
+            name: e.target.files[0].name
+        });
+    }
+    function removeImage() {
+        setImg({
+            img: "",
+            name:""
+        })
+    }
 
     return <>
         <div className='container'>
@@ -16,12 +34,12 @@ const navigate = useNavigate()
                     <div className="text">Create Survey</div>
                     <div className="util">
                         <div id="cancel-btn">
-                            <button onClick={()=>{
+                            <button onClick={() => {
                                 navigate('/list-survey')
                             }}>Cancel</button>
                         </div>
                         <div id="next-btn">
-                            <button onClick={()=>{
+                            <button onClick={() => {
                                 navigate('/list-survey/create/questions')
                             }}>Next</button>
                         </div>
@@ -67,10 +85,22 @@ const navigate = useNavigate()
                             </div>
                             <div>
                                 <label>Upload Image</label>
-                                <input type="file" id="file" name="image" />
-    
+
+
+                                {!img.img && <div className="image-upload-wrap">
+                                    <input className="file-upload-input" type='file' onChange={handleChange} accept="image/*" />
+                                    <div className="drag-text">
+                                        <h3>Click here to select Image</h3>
+                                    </div>
+                                </div>}
+                                {img.img && <div className="file-upload-content">
+                                    <img className="file-upload-image" src={img.img} alt="img" />
+                                    <div className="image-title-wrap">
+                                        <button type="button" onClick={removeImage} className="remove-image">Remove <span className="image-title">{img.name}</span></button>
+                                    </div>
+                                </div>}
                             </div>
-                           
+
                         </form>
                     </div>
                 </div>
