409bce8a9608c1bcac7ec98793c9344acab8f5eb
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-04-22 22:32:51 +0530

    Added signout button

--- FrontEnd/src/Pages/Header.js
+++ FrontEnd/src/Pages/Header.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/header.css'
+import '../styles/addQuiz.css'
 //import home from '../../public/images/home-run (1).svg'
 
 export default function Header(){
+    const [toggle, setToggle] = useState(false);
 
     return<>
         
@@ -11,10 +13,23 @@ export default function Header(){
                     <section className='logo'>Logo</section>
                     <section id='profile'>
                         <div id="profilepic"></div>
-                        <div className='dropdown'></div>
+                        <div onClick={()=>{setToggle(prevToggle=>!prevToggle)}} className='dropdown'></div>
                     </section>
                 </div>
+                
+                    <div className={`float_box margin ${(toggle)? "active":'inactive'}`} margin>
+                        <div>
 
+                            
+                                <div>
+                                    <button  id="signout-btn">SignOut</button>
+                                </div>
+                            
+                        </div>
+                        
+
+                    </div>
+                
             </header>
     </>
 }
